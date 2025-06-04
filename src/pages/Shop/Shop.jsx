41b6187991a6ac5d3b218bef1a912ff4f09b4d1b
Moustafa El Dehni <moustafa.eldehni@hotmail.com>
2025-06-05 00:01:51 +0200

    responsive mobile

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -6,6 +6,7 @@ export default function Shop({listeFavoris, setListeFavoris}) {
 
 
     const [show, setShow] = useState(false)
+    const [menu, setMenu] = useState(false)
 
 
 
@@ -20,11 +21,30 @@ export default function Shop({listeFavoris, setListeFavoris}) {
                     <Link className='disabledLink'><li>Bijoux</li></Link>
                     <Link className='disabledLink'><li>Électroniques</li></Link>
                 </ul>
-                <div style={{display: "flex", gap: "10px"}}>
+                <div style={{display: "flex", gap: "10px", alignItems: "center"}}>
                     <button onClick={()=> setShow(true)}>Mes Favoris</button>
                     <button>Panier</button>
+                    {menu ? "" : (
+                        <div onClick={()=> setMenu(true)} className='divBurger'><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="white"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></div>
+                    )}
                 </div>
             </nav>
+            {menu ? (
+                <div className='sideMenu'>
+                    <div onClick={()=> setMenu(false)} className='divClose'><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="white"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></div>
+                    <ul>
+                    <Link onClick={()=> setMenu(false)} to="/products"><li>Boutique</li></Link>
+                    <Link className='disabledLink'><li>Homme</li></Link>
+                    <Link className='disabledLink'><li>Femme</li></Link>
+                    <Link className='disabledLink'><li>Bijoux</li></Link>
+                    <Link className='disabledLink'><li>Électroniques</li></Link>
+                    <div style={{display: "flex", gap: "10px", alignItems: "center", flexDirection: "column"}}>
+                        <button onClick={()=> setShow(true)}>Mes Favoris</button>
+                        <button>Panier</button>
+                    </div>
+                </ul>
+                </div>
+            ):""}
             {show ? (
                 <div className='showFavoris'>
                     <button onClick={()=> setShow(false)}>Fermer</button>
